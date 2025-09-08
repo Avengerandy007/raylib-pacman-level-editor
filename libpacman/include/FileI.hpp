12c23001b95e771fee820d3be494f9417bbf3fb1
Avengerandy007 <Avengerandy007@users.noreply.github.com>
2025-09-08 17:44:07 +0300

    Updated to newest engine version

--- libpacman/include/FileI.hpp
+++ libpacman/include/FileI.hpp
@@ -6,5 +6,7 @@
 #include <fstream>
 namespace FileI{
 	extern std::ifstream FindFile();
+	extern std::string FileContent(std::ifstream& file);
+	extern Matrix2<Tile, 20> CreateTileSet(std::string& fileContent);
 	extern Matrix2<Tile, 20> MakeMatrix(std::ifstream& file);
 }
